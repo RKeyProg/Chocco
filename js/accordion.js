9ee11f24545c80b9fbd8fdb14cccb0f00f938003
Kirill Radkevich <rkey_prog@mail.ru>
2020-07-02 14:46:14 +0300

    ДОбавление слайдшоу и исправление аккордиона

--- js/accordion.js
+++ js/accordion.js
@@ -1,47 +1,34 @@
-$(document).ready(() => {
-    const teamList = $('.team__list');
-    const teamItem = teamList.find('.team__item');
+const openItem = (item, triang) => {
+    const container = item.closest('.team__item');
+    const content = container.find('.team__content');
+    const about = container.find('.team__about');
+    const reqHeight = about.height();
+    const triangle = container.find('.team__triangle');
 
-    if (window.innerWidth <= 768) { // Если на планшетах, то поменять местами title и photo
-        teamItem.each(function() {
-            const teamPhoto = $(this).find('.team__photo');
-            const teamClone = teamPhoto.clone();
+    container.addClass('team__item_active');
+    content.height(reqHeight);
+    triangle.css('transform', 'rotate(-180deg)');
+}
 
-            $(this).find('.team__title').after(teamClone);
-            teamPhoto.remove();
-        })
-    }
-    
-    teamList.on('click', e => {
-        const teamLink = $(e.target);
+const closeEveryItem = container => {
+    const itemcontainer = container.find('.team__item');
+    const items = container.find('.team__content');
+    const triangles = container.find('.team__triangle');
 
-        if ($(teamLink).hasClass('team__btn')) {
-            const isActiveBefore = teamList.find('.team__btn.is-active'); // элементы с классом is-active
-            const teamPhotoAccordion = teamLink.closest('.team__title').siblings('.team__photo'); // поиск фотографии 
-            const teamPhotoAccordionActiv = isActiveBefore.closest('.team__title').siblings('.team__photo'); // поиск фотоографии с активной кнопкой
-            
-            if (isActiveBefore.length != 0) {//закрытие
-                if (window.innerWidth <= 768) {
-                    teamPhotoAccordionActiv.css('height', 0);
-                }
-                isActiveBefore.css('transform', 'rotate(0deg)');
-                isActiveBefore.toggleClass('is-active');// удаление класса is-active
-                isActiveBefore.closest('.team__title').siblings('.team__about').css('height', 0); // обнуление высоты у элемента класса team__about, который в одном пункте с кнопкой is-active
-            }
-            
-            const teamAbout = teamLink.closest('.team__title').siblings('.team__about'); // элемент, который надо развернуть
-            const teamAboutHeight = teamAbout[0].scrollHeight; // высота контента элемента, который надо развернуть
+    itemcontainer.removeClass('team__item_active');
+    items.height(0);
+    triangles.css('transform', 'rotate(0deg)');
+}
 
-            if (isActiveBefore.length == 0 || isActiveBefore[0] !== teamLink[0]) { //открытие
-                if (window.innerWidth <= 480) {
-                    teamPhotoAccordion.css('height', "11rem");
-                } else if (window.innerWidth <= 768) {
-                    teamPhotoAccordion.css('height', "16rem");
-                }
-                teamLink.css('transform', 'rotate(-180deg)');
-                teamLink.toggleClass('is-active'); // добавление класса is-active
-                teamAbout.css('height', teamAboutHeight); // установка высоты
-            }
-        }
-    })
+$('.team__title').on('click', e => {
+   const $this = $(e.currentTarget);
+   const container = $this.closest('.team__list');
+   const elemContainer = $this.closest('.team__item');
+
+   if (elemContainer.hasClass('team__item_active')) {
+       closeEveryItem(container);
+   } else {    
+       closeEveryItem(container);
+       openItem($this);
+   }
 })
